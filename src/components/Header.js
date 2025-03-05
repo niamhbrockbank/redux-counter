9e2252a97930f3a07331d37a4013fce167ba7f40
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-03-05 12:45:08 +0000

    Configure store with multiple slices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
-import classes from './Header.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Header.module.css";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+            <a href="/">My Products</a>
           </li>
           <li>
-            <a href='/'>My Sales</a>
+            <a href="/">My Sales</a>
           </li>
           <li>
             <button>Logout</button>
